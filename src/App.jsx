90bce776ecffa37736763dd85a60fa43702efa76
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-10-25 09:06:39 +0530

    create todoList using by redux toolKit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./component/AddTodo";
+import Todos from "./component/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
